8d3925765466ca29645b95b3cd0c5d61ecc19097
Joel Ye <jye5@stuy.edu>
2016-09-23 14:18:29 -0400

    Add files

---
+++ webGen.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import random, occupation
+
+app = Flask(__name__)
+
+@app.route("/")
+def homePage():
+    return render_template('home.html', title = "Home", head = "Directory", links = ['/occupation'])
+
+@app.route("/occupation")
+def occupationPage():    
+    header = "Occupation Database"
+    return render_template('occupation.html', title = "Occupations", head = header, links = ['/'], table = occupation.getD(), randChoice = occupation.randSelect())
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
